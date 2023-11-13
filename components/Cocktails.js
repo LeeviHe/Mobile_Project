d4d1dbd03013b9e8fa2a0dab0ea3cfd30ed36904
LeeviHe <l.helttunen@gmail.com>
2023-11-13 17:52:30 +0200

    Components and styles files created

---
+++ components/Cocktails.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Cocktails() {
+  return (
+    <View style={styles.container}>
+      <Text>Cocktails</Text>
+    </View>
+  );
+}
---
+++ components/Ingredients.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Ingredients() {
+  return (
+    <View style={styles.container}>
+      <Text>Ingredients</Text>
+    </View>
+  );
+}
