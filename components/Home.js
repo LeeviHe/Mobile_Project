ae00588751226fbb11f9fc1803be8d5dd3e811d4
MiaaaV <sirivesa@gmail.com>
2023-11-21 19:44:00 +0200

    carousel done and styled, logo edits, imgs added

--- components/Home.js
+++ components/Home.js
@@ -1,68 +1,117 @@
-import { useEffect, useState } from 'react';
-import { ScrollView, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Text, View, Image, ImageBackground } from 'react-native';
 import { Container, Row, Col } from "react-native-flex-grid";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { PageSlider } from '@pietile-native-kit/page-slider';
+import styles from '../styles/styles'
+import { useFonts } from 'expo-font';
 
 const URL = 'https://www.thecocktaildb.com/api/json/v2/9973533/';
 const favIcon = '../assets/defaults/favicon.png'
 
-export default function Home() {
+function Home() {
+    const [selectedPage, setSelectedPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const [font] = useFonts({
+        Montserrat: require('../assets/fonts/Montserrat/static/Montserrat-Regular.ttf'),
+        Raleway: require('../assets/fonts/Raleway/static/Raleway-SemiBold.ttf')
+    })
+
+    if (!font) {
+        return null;
+    }
+
 
     return (
         <ScrollView>
-            <View style={{ backgroundColor: '#f5f5f5' }}>
-                <View>
-                    <Image source={require('../assets/logos/pockettini-logo-regular-256px-black.png')} style={{ alignSelf: 'center', marginTop: 100 }} />
-                </View>
-                <View>
-                    <Text style={{ paddingTop: 100, paddingLeft: 100 }}>Carousel here</Text>
-                </View>
-                <View>
-                    <Text style={{ paddingTop: 100, paddingLeft: 100, fontSize: 28 }}>Discover</Text>
-                </View>
-                <View style={{ paddingTop: 100, paddingLeft: 100, paddingRight: 100 }}>
-                    <Row>
-                        <Col style={{ backgroundColor: 'lightyellow' }}>
-                            <Text>Surprise me!</Text>
-                            <Text style={{ fontSize: 10 }}>Description</Text>
-                            <Image
-                                source={require(favIcon)}
-                            />
-                        </Col>
-                        <Col style={{ backgroundColor: 'pink' }}>
-                            <Text>Coffee & tea</Text>
-                            <Text style={{ fontSize: 10 }}>Description</Text>
-                            <Image
-                                source={require(favIcon)}
-                            />
-                        </Col>
-                    </Row>
+
+            <View style={styles.viewHelper}>
+                <ImageBackground source={require('../assets/images/Carousel-cocktails-2.png')}
+                    style={styles.pageBackground} resizeMode='cover'>
+                    <View style={{ marginBottom: 50 }}>
+                        <Image source={require('../assets/logos/pockettini-logo-regular-256px-black.png')}
+                            style={styles.logo} />
+                    </View>
+
+
                     <View>
+                        <PageSlider
+                            style={styles.pageSlider}
+                            selectedPage={1}
+                            onSelectedPageChange={setSelectedPage}
+                            mode='card'
+                            peek={50}
+                            pageMargin={-10}
+                            onCurrentPageChange={setCurrentPage}>
+
+                            <View style={styles.page}>
+                                <Image style={styles.carouselImage} source={require('../assets/images/Carousel-cocktails-1.png')} />
+                                <Text style={styles.H1}>Devs' Favourites</Text>
+                            </View>
+
+                            <View style={styles.page}>
+                                <Image style={styles.carouselImage} source={require('../assets/images/Carousel-cocktails-2.png')} />
+                                <Text style={styles.H1}>Most Popular</Text>
+                            </View>
+
+                            <View style={styles.page}>
+                                <Image style={styles.carouselImage} source={require('../assets/images/Carousel-cocktails-3.png')} />
+                                <Text style={styles.H1}>Latest Recipes</Text>
+                            </View>
+
+                        </PageSlider>
+                    </View>
+
+                    <View>
+                        <Text style={{ paddingTop: 100, paddingLeft: 100, fontSize: 28 }}>Discover</Text>
+                    </View>
+                    <View style={{ paddingTop: 100, paddingLeft: 100, paddingRight: 100 }}>
                         <Row>
-                            <Col style={{ backgroundColor: 'lightgreen' }}>
-                                <Text>Alcohol free</Text>
+                            <Col style={{ backgroundColor: 'lightyellow' }}>
+                                <Text>Surprise me!</Text>
                                 <Text style={{ fontSize: 10 }}>Description</Text>
                                 <Image
                                     source={require(favIcon)}
                                 />
                             </Col>
-                            <Col style={{ backgroundColor: 'lightblue' }}>
-                                <Text>Alcoholic drinks</Text>
+                            <Col style={{ backgroundColor: 'pink' }}>
+                                <Text>Coffee & tea</Text>
                                 <Text style={{ fontSize: 10 }}>Description</Text>
                                 <Image
                                     source={require(favIcon)}
                                 />
                             </Col>
                         </Row>
-                    </View>
+                        <View>
+                            <Row>
+                                <Col style={{ backgroundColor: 'lightgreen' }}>
+                                    <Text>Alcohol free</Text>
+                                    <Text style={{ fontSize: 10 }}>Description</Text>
+                                    <Image
+                                        source={require(favIcon)}
+                                    />
+                                </Col>
+                                <Col style={{ backgroundColor: 'lightblue' }}>
+                                    <Text>Alcoholic drinks</Text>
+                                    <Text style={{ fontSize: 10 }}>Description</Text>
+                                    <Image
+                                        source={require(favIcon)}
+                                    />
+                                </Col>
+                            </Row>
+                        </View>
 
-                </View>
-                <View>
-                    <Text style={{ paddingTop: 100, paddingLeft: 50 }}>MAP</Text>
-                    <Icon name="map" style={{ paddingTop: 50, paddingLeft: 150, fontSize: 100 }}></Icon>
-                </View>
+                    </View>
+                    <View>
+                        <Text style={{ paddingTop: 100, paddingLeft: 50 }}>MAP</Text>
+                        <Icon name="map" style={{ paddingTop: 50, paddingLeft: 150, fontSize: 100 }}></Icon>
+                    </View>
+                </ImageBackground>
             </View>
-        </ScrollView>
+        </ScrollView >
 
     );
 }
+
+export default Home;
---
+++ styles/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  viewHelper: {
+    marginTop: 70
+  },
+  pageBackground: {
+    maxWidth: '100%'
+  },
+  logo: {
+    alignSelf: "center",
+    width: 240,
+    height: 50
+  },
+  pageSlider: {
+    width: '100%',
+    //backgroundColor: '#e580802d'
+  },
+  carouselImage: {
+    width: '80%',
+    height: 150
+  },
+  H1: {
+    color: '#5A5A5A',
+    fontFamily: 'Raleway',
+    fontSize: 20,
+    marginTop: 20
+  },
+  page: {
+    width: '100%',
+    height: 210,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
