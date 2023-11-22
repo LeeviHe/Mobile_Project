a9462e06f992e7daaa666d4e137b280bb249cec1
MiaaaV <sirivesa@gmail.com>
2023-11-22 20:41:42 +0200

    renamed 'assistant', bottom tab changes

---
+++ components/More.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function More() {
+  return (
+    <View>
+      <Text style={{ paddingTop: 100, paddingLeft: 120, fontSize: 28 }}>More</Text>
+    </View>
+  );
+}
