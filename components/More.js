26e166442f79a0457d9ddbcd732db8a01ba11689
MiaaaV <sirivesa@gmail.com>
2023-12-05 19:43:33 +0200

    more page layout ok

--- components/More.js
+++ components/More.js
@@ -1,9 +1,21 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { colors } from '../styles/style-constants';
+import styles from '../styles/styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function More() {
   return (
-    <View>
-      <Text style={{ paddingTop: 100, paddingLeft: 120, fontSize: 28 }}>More</Text>
-    </View>
+    <View style={styles.moreContainer}>
+
+      <TouchableOpacity style={styles.moreItem}>
+        <Icon name='heart' size={40} color={colors.mainFontColour} />
+        <Text style={styles.moreText}>Favourites</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.moreItem}>
+        <Icon name='note-text' size={40} color={colors.mainFontColour} />
+        <Text style={styles.moreText}>My Pockettinis</Text>
+      </TouchableOpacity>
+    </View >
   );
 }
