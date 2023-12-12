24b21cd49f3af61f212480d5fa0b5638b3f26715
MiaaaV <sirivesa@gmail.com>
2023-12-12 15:53:05 +0200

    replaced alert with modal

--- components/Favourites.js
+++ components/Favourites.js
@@ -1,48 +1,40 @@
-import { Text, View, TouchableOpacity, Image, FlatList, ActivityIndicator} from 'react-native';
+import { Text, View, TouchableOpacity, Image, FlatList, ActivityIndicator, Modal } from 'react-native';
 import styles from '../styles/styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useState, useEffect } from 'react';
-import { colors, fonts, padding, textStyles } from '../styles/style-constants';
+import { colors, fonts, padding, textStyles, modalStyle } from '../styles/style-constants';
 import { FAVOURITE_DRINKS_KEY } from '../reusables/Constants';
 import { URL } from '../reusables/Constants';
-import { getJsonDrinks} from '../reusables/Functions';
+import { getJsonDrinks } from '../reusables/Functions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button } from 'react-native-paper';
 import { ScrollView } from 'react-native-virtualized-view';
 import { useGlobalState } from '../reusables/Functions';
 
-export default function Favourites({ navigation , route }) {
+export default function Favourites({ navigation, route }) {
   const [favouritesIds, setFavouritesIds] = useState([])
   const [favourites, setFavourites] = useState([])
   const [temp, setTemp] = useState([])
   const { asyncStorageData } = useGlobalState()
   const [newInfo, setNewInfo] = useState([])
   const [isAsyncbusy, setAsyncBusy] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [modalText, setModalText] = useState('')
+  const [linkText, setLinkText] = useState('')
 
-  async function clear() {
-    try { 
-      await AsyncStorage.removeItem(FAVOURITE_DRINKS_KEY)
-      setFavouritesIds([])
-      setFavourites([])
-    } catch (e) {
-        console.log('Clear error: ' + e)
-    }
-  }
-
-  useEffect (() => {
+  useEffect(() => {
     const unsubsribe = navigation.addListener('focus', async () => {
       favouriteData()
     })
-    return () => {unsubsribe()}
+    return () => { unsubsribe() }
   }, [asyncStorageData, navigation])
 
 
-  useEffect (() => {
+  useEffect(() => {
     const fetchData = async () => {
       if (favouritesIds) {
         setAsyncBusy(true)
-        const fetchPromises = favouritesIds.map(fav => 
-          getJsonDrinks(URL,'lookup.php?i=' + fav.drinkId, setTemp)
+        const fetchPromises = favouritesIds.map(fav =>
+          getJsonDrinks(URL, 'lookup.php?i=' + fav.drinkId, setTemp)
         )
         try {
           const results = await Promise.all(fetchPromises)
@@ -53,7 +45,7 @@ export default function Favourites({ navigation , route }) {
             console.log('No tempData')
             return
           }
-        } catch(e) {
+        } catch (e) {
           console.log('error fetching and updating')
         }
       }
@@ -62,7 +54,7 @@ export default function Favourites({ navigation , route }) {
     fetchData()
   }, [favouritesIds])
 
-  useEffect (() => {
+  useEffect(() => {
     if (newInfo.length > 0) {
       setFavourites(newInfo)
     }
@@ -71,17 +63,17 @@ export default function Favourites({ navigation , route }) {
 
   const favouriteData = async () => {
     try {
-        const jsonValue = await AsyncStorage.getItem(FAVOURITE_DRINKS_KEY)
-        if (jsonValue !== null) {
-            let data = JSON.parse(jsonValue)
-            setFavouritesIds(data)
-        }
+      const jsonValue = await AsyncStorage.getItem(FAVOURITE_DRINKS_KEY)
+      if (jsonValue !== null) {
+        let data = JSON.parse(jsonValue)
+        setFavouritesIds(data)
+      }
     }
     catch (e) {
-        console.log('Read error: ' + e)
+      console.log('Read error: ' + e)
     }
   }
-  
+
   const isAlcoholic = (category) => {
     const alcoholicCategories = [
       'Ordinary Drink',
@@ -103,10 +95,10 @@ export default function Favourites({ navigation , route }) {
 
     return nonAlcoholicCategories.includes(category)
   }
-  
+
   const renderDrinkItem = ({ item, index }) => {
     const isFavourited = true
-    const toggleHeart = async() => {
+    const toggleHeart = async () => {
       try {
         if (isFavourited) {
           const newFavourites = favourites.filter((fav) => fav.drinkId !== item.idDrink)
@@ -114,12 +106,17 @@ export default function Favourites({ navigation , route }) {
           const newFavouritesIds = favouritesIds.filter((fav) => fav.drinkId !== item.idDrink);
           setFavouritesIds(newFavouritesIds);
           await AsyncStorage.setItem(FAVOURITE_DRINKS_KEY, JSON.stringify(newFavouritesIds))
-          alert('Drink removed')
+          setModal(true)
+          setModalText('Removed from favourites')
+          setLinkText('')
+          setTimeout(() => {
+            setModal(false)
+          }, 1000);
         }
-      } catch(error) {
+      } catch (error) {
         console.log('Error saving favourite: ' + error)
-        setFavourites((prevFavourites) => 
-        prevFavourites.filter((fav) => fav.drinkId !== item.idDrink)
+        setFavourites((prevFavourites) =>
+          prevFavourites.filter((fav) => fav.drinkId !== item.idDrink)
         )
       }
     };
@@ -151,16 +148,18 @@ export default function Favourites({ navigation , route }) {
           key={index}
           style={[styles.cocktail, { backgroundColor: categoryBackgroundColor() }]}
           onPress={() =>
-            navigation.navigate('CocktailsNavigator', {screen: 'Recipe', params: {
-              drinkId: item.idDrink,
-              drinkName: item.strDrink,
-              image: item.strDrinkThumb,
-              category: item.strCategory,
-              glass: item.strGlass,
-              instructions: item.strInstructions,
-              navigator: 'MoreNavigator',
-              screen: 'Favourites'
-            }})
+            navigation.navigate('CocktailsNavigator', {
+              screen: 'Recipe', params: {
+                drinkId: item.idDrink,
+                drinkName: item.strDrink,
+                image: item.strDrinkThumb,
+                category: item.strCategory,
+                glass: item.strGlass,
+                instructions: item.strInstructions,
+                navigator: 'MoreNavigator',
+                screen: 'Favourites'
+              }
+            })
           }>
 
           <View style={[styles.cocktailInfo, { flexDirection: 'row', alignItems: 'center' }]}>
@@ -190,22 +189,55 @@ export default function Favourites({ navigation , route }) {
   }
 
   return (
-    <ScrollView style={{ backgroundColor: colors.white}}>
+    <ScrollView style={{ backgroundColor: colors.white }}>
       <Text style={[textStyles.pageTitle, textStyles.spacingHelp]}>My Favourites</Text>
       <View style={styles.favBtnContainer}>
-        <Button icon="camera"onPress={() =>clear()} />
         {!isAsyncbusy ? (<>
-        { favouritesIds.length === 0 ? 
-        <Text>No favourites saved</Text> 
-        :
-          <FlatList
-            data={favourites}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={renderDrinkItem}
-            extraData={favourites}
-          />
-        }</>):(<ActivityIndicator size={250} color={"#c0c0c0"}/>)}
-        </View>
+          {favouritesIds.length === 0 ?
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ fontFamily: fonts.text, color: colors.mainFontColour }}>No favourites saved!</Text>
+              <Text style={{ fontFamily: fonts.text, color: colors.mainFontColour }}>Your favourited items will show here.</Text>
+            </View>
+            :
+            <FlatList
+              data={favourites}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={renderDrinkItem}
+              extraData={favourites}
+            />
+          }</>) : (<ActivityIndicator size={250} color={"#c0c0c0"} />)}
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modal}
+          onRequestClose={() => {
+            setModal(!modal);
+          }}>
+
+          <View style={modalStyle.container}>
+            <View style={modalStyle.view}>
+
+              <Text style={modalStyle.text}>
+                {modalText}
+                {linkText ? (
+                  <Text
+                    style={[modalStyle.linkText, { textDecorationLine: 'underline' }]}
+                    onPress={navToFav}>
+                    {linkText}
+                  </Text>
+                ) : null}
+              </Text>
+
+              <TouchableOpacity
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModal(!modal)}>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+      </View>
     </ScrollView>
   );
 }
